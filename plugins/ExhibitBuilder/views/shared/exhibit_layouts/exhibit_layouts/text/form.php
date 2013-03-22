2023381b8322503bf64ac5cce9e58946d22ea223
Anton Yazovskiy <yazovsky@gmail.com>
2013-03-22 09:13:24 -0400

    clean

--- plugins/ExhibitBuilder/views/shared/exhibit_layouts/exhibit_layouts/text/form.php
+++
@@ -1,5 +0,0 @@
-<fieldset>
-    <?php
-        echo exhibit_builder_layout_form_text(1);
-    ?>
-</fieldset>
--- plugins/ExhibitBuilder/views/shared/exhibit_layouts/exhibit_layouts/text/layout.php
+++
@@ -1,7 +0,0 @@
-<div class="text-full">
-    <div class="primary">
-        <div class="exhibit-text">
-            <?php echo exhibit_builder_page_text(1); ?>
-        </div>
-    </div>
-</div>
