e1b92020d6b0c9f6d73da5921dbf7405122e3ccc
Anton Yazovskiy <yazovsky@gmail.com>
2013-03-23 15:46:39 -0400

    make exhibit home page layout empty because this layout isn't for 'real' pages. Page with "DPLA Exhibit Homepage" layour is container of exhibit meta-information (thumbnail, thumbnail caption and exhibit description)

--- plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-exhibit-home-page/layout.php
+++ plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-exhibit-home-page/layout.php
@@ -1,24 +1,25 @@
-<? if ($attachment = exhibit_builder_page_attachment(2)): ?>
-    <div class="slide-Container">
-        <div class="slidegallery">
-            <div class="slides">
-            <section id="slideshow">
-                <? echo dpla_attachment_markup($attachment, array('imageSize' => 'fullsize'), array('class' => 'permalink')); ?>
-            </section>
-            </div>
-            <? if ($gallery = dpla_thumbnail_gallery(2, 7, array('class'=>'permalink'))): ?>
-                <div id="thumbs"><?= $gallery ?></div>
-            <? endif; ?>
-        </div>
-    </div>
-<? endif; ?>
-
-<?php echo exhibit_builder_page_text(2); ?>
-
-<ul class="prevNext">
-    <?php // TODO: Define first and last pages ?>
-    <? if ($nextLink = dpla_link_to_next_page('Next »')): ?>
-        <li class="btn"><?= $nextLink ?></li>
-    <? endif; ?>
-    <li><?= dpla_page_position(); ?></li>
-</ul>
+<?// if ($attachment = exhibit_builder_page_attachment(2)): ?>
+<!--    <div class="slide-Container">-->
+<!--        <div class="slidegallery">-->
+<!--            <div class="slides">-->
+<!--            <section id="slideshow">-->
+<!--                --><?// echo dpla_attachment_markup($attachment, array('imageSize' => 'fullsize'), array('class' => 'permalink')); ?>
+<!--            </section>-->
+<!--            </div>-->
+<!--            --><?// if ($gallery = dpla_thumbnail_gallery(2, 7, array('class'=>'permalink'))): ?>
+<!--                <div id="thumbs">--><?//= $gallery ?><!--</div>-->
+<!--            --><?// endif; ?>
+<!--        </div>-->
+<!--    </div>-->
+<?// endif; ?>
+<!---->
+<?php //echo exhibit_builder_page_text(2); ?>
+<!---->
+<!--<ul class="prevNext">-->
+<!--    --><?php //// TODO: Define first and last pages ?>
+<!--    --><?// if ($nextLink = dpla_link_to_next_page('Next »')): ?>
+<!--        <li class="btn">--><?//= $nextLink ?><!--</li>-->
+<!--    --><?// endif; ?>
+<!--    <li>--><?//= dpla_page_position(); ?><!--</li>-->
+<!--</ul>-->
+<span>meta-page without content</span>
