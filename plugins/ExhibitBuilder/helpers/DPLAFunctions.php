a617e53a229afa37826b519e1908a9b2fe3710e4
Anton Yazovskiy <yazovsky@gmail.com>
2013-03-27 23:36:08 -0400

    add FIXME comment

--- plugins/ExhibitBuilder/helpers/DPLAFunctions.php
+++ plugins/ExhibitBuilder/helpers/DPLAFunctions.php
@@ -317,10 +317,12 @@ function get_attachment_thumbnail($attachment, $type = "square_thumbnail") {
     if (isset($attachment['file'])) {
         $uri = $attachment['file']->getWebPath($type);
         if (!file_exists("files/".$attachment['file']->getStoragePath($type))) {
+            // FIXME: clean up this
             $uri = "http://openexhibits.org/wp-content/uploads/icon/large/video-viewer-icon-100x100.png";
         }
     }
     if (!isset($uri) || !$uri) {
+        // FIXME: clean up this
         $uri = "http://openexhibits.org/wp-content/uploads/icon/large/video-viewer-icon-100x100.png";
     }
     return $uri;
