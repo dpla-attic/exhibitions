6b08376daa5b085fe21a1736bd9b4ba7c394a2a2
Anton Yazovskiy <yazovsky@gmail.com>
2013-03-27 22:15:32 -0400

    display media plugins: Zoom.It plugin: logging fixed

--- plugins/Zoomit/views/shared/common/zoomit.php
+++ plugins/Zoomit/views/shared/common/zoomit.php
@@ -3,7 +3,6 @@ jQuery(document).ready(function () {
 
     var zoomitParent = jQuery('.zoomit');
 
-    console.log(zoomitParent.length);
     zoomitParent.each(function (parent) {
         var zoomit = jQuery(this).find('.zoomit_viewer').first();
 
@@ -20,10 +19,11 @@ jQuery(document).ready(function () {
                     + 'width="100%" '
                     + 'height="420"></iframe>'
             );
-            console.log("ZoomIt: Before call GET service");
+            console.log("ZoomIt: Before call GET service: " + this.href);
             jQuery.get('<?php echo ZoomitPlugin::API_URL; ?>?url=' + encodeURIComponent(this.href), function(data) {
                 console.log("ZoomIt: After call GET service");
                 var iframe = jQuery(zoomit).find('.zoomit_iframe')[0].contentWindow.document;
+
                 // this may be already loaded
                 if (iframe.innerHTML != '') {
                     iframe.open();
