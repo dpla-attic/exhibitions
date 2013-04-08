0415618da4827c4d5fa05411bddda2849c5fe8b2
Anton Yazovskiy <yazovsky@gmail.com>
2013-04-08 02:43:09 -0400

    #21478 browse exhibitions: add pagination

--- themes/dpla/exhibit-builder/exhibits/browse.php
+++ themes/dpla/exhibit-builder/exhibits/browse.php
@@ -2,73 +2,113 @@
 $title = __('Exhibitions');
 echo head(array('title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'browse'));
 ?>
-<article>
+<article id="exhibits-container">
     <?php fire_plugin_hook('public_content_top'); ?>
 
     <ul class="shareSave">
         <li class="btn">
-	        <a href="">Share</a>
-			<ul>
-		        <li><div class="sharebtn"><div id="fb-root"></div><div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></div></li>
-		        <li><div class="sharebtn"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div></li>
-		        <li><div class="sharebtn"><div class="g-plusone" data-size="medium"></div></div></li>
-	        </ul>
+            <a href="">Share</a>
+            <ul>
+                <li>
+                    <div class="sharebtn">
+                        <div id="fb-root"></div>
+                        <div class="fb-like" data-send="false" data-layout="button_count" data-width="450"
+                             data-show-faces="false"></div>
+                    </div>
+                </li>
+                <li>
+                    <div class="sharebtn"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+                        <script>!function (d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (!d.getElementById(id)) {
+                                    js = d.createElement(s);
+                                    js.id = id;
+                                    js.src = "//platform.twitter.com/widgets.js";
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }
+                            }(document, "script", "twitter-wjs");</script>
+                    </div>
+                </li>
+                <li>
+                    <div class="sharebtn">
+                        <div class="g-plusone" data-size="medium"></div>
+                    </div>
+                </li>
+            </ul>
         </li>
     </ul>
-	<h1 class="border"><?php echo $title; ?></h1>
-	<h4><?php echo option('description');?></h4>
-	<?php if (count($exhibits) > 0): ?>
+    <h1 class="border"><?php echo $title; ?></h1>
+    <h4><?php echo option('description'); ?></h4>
+    <?php if (count($exhibits) > 0): ?>
 
-	<?php echo pagination_links(); ?>
-
-	<?php $exhibitCount = 0; ?>
-	<div class="moduleContainer threeCol">
-	<?php foreach (loop('exhibit') as $exhibit): ?>
-	    <?php $exhibitCount++; ?>
-
-	    
-
-	    <section class="module blue <?php if ($exhibitCount%2==1) echo ' even'; else echo ' odd'; ?>">
-            <?php
-            if ($homepage = dpla_get_exhibit_homepage($exhibit)) {
-                if ($att = dpla_exhibit_page_thumbnail_att($homepage)) {
-                    $thumbUri = $att['file_uri_notsquare'];
+        <?php $page_size = (int) Zend_Registry::get('bootstrap')->getResource('Config')->dpla->exhibit_page_size;
+        if (total_records('exhibit') > $page_size) :?>
+            <div class="pagination">
+                <span data-page="1" class="current">1</span>
+                <?php for ($i = 2; $i <= (total_records('exhibit') / $page_size) + 1; $i++) {
+                    echo "<a data-page='" . $i . "' href='#' data-page='" . $i . "'>" . $i . "</a>";
                 }
-            }
-            ?>
-            <div class="exibition-image">
-                <a href="<?=exhibit_builder_exhibit_uri()?>">
-            	    <img alt="img" src="<?=$thumbUri?>"></img>
-                </a>
+                ?>
             </div>
-	        <h5><?php echo link_to_exhibit(); ?></h5>
-            <?php
-            // exhibit description should be taken from exhibit Homepage
-            if ($homepage) {
-                if ($text = exhibit_builder_page_text(2, $homepage)) {
-                    if(strlen($text) > 120) $text = substr($text, 0, 120).'...';
-                } else if ($text = exhibit_builder_page_text(1, $homepage)) { // prefer Long description to Short
-                    if(strlen($text) > 120) $text = substr($text, 0, 120).'...';
-                }
-                echo $text ? "<p>".$text."</p>" : "";
-            }
-            ?>
-	        <?php echo link_to_exhibit('View Exhibit »'); ?>
-	    </section>
+        <?php endif; ?>
 
-		<?php // TODO: Find a better way to make rows of 3 items ?>
-		<?php if (($exhibitCount%3==0) && $exhibitCount < count($exhibits)): ?>
-		</div>
-		<div class="moduleContainer threeCol">
-		<?php endif; ?>
+        <?php $exhibitCount = 0; ?>
+        <div class='pop-columns' data-page='1'>
+            <div class="moduleContainer threeCol">
+                <?php foreach (loop('exhibit') as $exhibit): ?>
+                <?php $exhibitCount++; ?>
 
-	<?php endforeach; ?>
-	</div>
-	<?php echo pagination_links(); ?>
+                <section class="module blue <?php if ($exhibitCount % 2 == 1) echo ' even'; else echo ' odd'; ?>">
+                    <?php
+                    if ($homepage = dpla_get_exhibit_homepage($exhibit)) {
+                        if ($att = dpla_exhibit_page_thumbnail_att($homepage)) {
+                            $thumbUri = $att['file_uri_notsquare'];
+                        }
+                    }
+                    ?>
+                    <div class="exibition-image">
+                        <a href="<?= exhibit_builder_exhibit_uri() ?>">
+                            <img alt="img" src="<?= $thumbUri ?>"></img>
+                        </a>
+                    </div>
+                    <h5><?php echo link_to_exhibit(); ?></h5>
+                    <?php
+                    // exhibit description should be taken from exhibit Homepage
+                    if ($homepage) {
+                        if ($text = exhibit_builder_page_text(2, $homepage)) {
+                            if (strlen($text) > 120) $text = substr($text, 0, 120) . '...';
+                        } else if ($text = exhibit_builder_page_text(1, $homepage)) { // prefer Long description to Short
+                            if (strlen($text) > 120) $text = substr($text, 0, 120) . '...';
+                        }
+                        echo $text ? "<p>" . $text . "</p>" : "";
+                    }
+                    ?>
+                    <?php echo link_to_exhibit('View Exhibit »'); ?>
+                </section>
 
-	<?php else: ?>
-	<p><?php echo __('There are no exhibits available yet.'); ?></p>
-	<?php endif; ?>
+                <?php // TODO: Find a better way to make rows of 3 items ?>
+                <?php if (($exhibitCount % 3 == 0) && $exhibitCount < count($exhibits)): ?>
+            </div>
+
+                <?php if (($exhibitCount % $page_size == 0) && $exhibitCount < count($exhibits)): ?>
+            </div>
+            <div class='pop-columns' style='display:none;' data-page='<?= ($exhibitCount / $page_size) + 1 ?>'>
+                <?php endif; ?>
+
+            <div class="moduleContainer threeCol">
+                <?php endif; ?>
+
+            <?php // TODO: Find a better way to make items pagination ?>
+
+
+
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <?php echo pagination_links(); ?>
+
+    <?php else: ?>
+        <p><?php echo __('There are no exhibits available yet.'); ?></p>
+    <?php endif; ?>
 </article>
 <?php echo foot(); ?>
