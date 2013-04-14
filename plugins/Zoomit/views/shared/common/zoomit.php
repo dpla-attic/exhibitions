1b2c4499421f9d2258cfd9e64075fd28e09bd9d6
Anton Yazovskiy <yazovsky@gmail.com>
2013-04-14 13:37:30 -0400

    add "param=dpla_prod" to zoomit calls to update service cache

--- plugins/Zoomit/views/shared/common/zoomit.php
+++ plugins/Zoomit/views/shared/common/zoomit.php
@@ -4,7 +4,7 @@ $unique_id = "zoomit_".hash("md4", $images[0]->getWebPath('original'))
 
 <div class="zoomit" id="<?=$unique_id?>" data-api-url="<?php echo ZoomitPlugin::API_URL; ?>">
     <?php foreach($images as $image): ?>
-    <a href="<?php echo html_escape($image->getWebPath('original')); ?>" class="zoomit_images"><?php echo html_escape($image->original_filename); ?></a>
+    <a href="<?php echo html_escape($image->getWebPath('original'))."?param=".html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->zoomit->update_cache_param); ?>" class="zoomit_images"><?php echo html_escape($image->original_filename); ?></a>
     <?php endforeach; ?>
     <div class="zoomit_viewer"></div>
 </div>
