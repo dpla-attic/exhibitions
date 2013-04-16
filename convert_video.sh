391f99924779e0cb28add899c95648dc05708540
Mekhanikov Evgeniy <emekhanikov@thumbtack.net>
2013-04-16 18:31:16 +0700

    Bug #24092: I have created convert_video.sh script to conversion mpeg4 files to h264.

---
+++ convert_video.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+SCAN_DIR=/media/sf_prj/*
+#FORBIDDEN_CODECS="mpeg4 h264"
+FORBIDDEN_CODECS="mpeg4"
+
+isConvertRequired () {
+    file=$1
+    fileInfo=$(ffmpeg -i $file 2>/dev/stdout)
+    for forbidden_codec in $FORBIDDEN_CODECS
+    do
+        flag=$(test "${fileInfo#*$forbidden_codec}" != "$fileInfo" && echo "1")
+        if [ "$flag" != "" ]; then
+           return "1";
+        fi
+    done
+    return "0"
+}
+
+convert () {
+    fileIn=$1
+    ext=${fileIn##*.}
+    fileOut="$fileIn.tmp.$ext"
+#    rm $fileOut -f
+    ffmpeg -loglevel panic -i $fileIn -vcodec libx264 $fileOut 2>/dev/null
+    if [ -f "$fileOut" ]
+        then 
+            mv $fileOut $fileIn -f
+            echo "success."
+        else 
+            echo "failed."
+    fi
+}
+
+echo "Processing directiry '$SCAN_DIR'"
+for f in $SCAN_DIR
+do
+    #echo "checking file - $f"
+    isConvertRequired $f
+    res=$?
+    if [ "$res" -eq "1" ]; then
+        echo "converting file '$f', please wait..."
+        convert $f
+    fi
+done
