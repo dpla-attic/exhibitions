92cb2f3d0e5d195eb06ca2010b5ee98796e321b7
Alex Smelyakov <asmelyakov@thumbtack.net>
2013-04-16 13:07:04 +0700

    bug #24095 ZoomIt refactoring

--- plugins/Zoomit/views/shared/common/zoomit.php
+++ plugins/Zoomit/views/shared/common/zoomit.php
@@ -3,8 +3,9 @@ $unique_id = "zoomit_".hash("md4", $images[0]->getWebPath('original'))
 ?>
 
 <div class="zoomit" id="<?=$unique_id?>" data-api-url="<?php echo ZoomitPlugin::API_URL; ?>">
+    <div class="zoomit_viewer">
     <?php foreach($images as $image): ?>
-    <a href="<?php echo html_escape($image->getWebPath('original'))."?param=".html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->zoomit->update_cache_param); ?>" class="zoomit_images"><?php echo html_escape($image->original_filename); ?></a>
+    	<img src="<?php echo html_escape($image->getWebPath('original'))."?param=".html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->zoomit->update_cache_param); ?>" data-original="<?php echo html_escape($image->getWebPath('original')); ?>" class="zoomit-image tmp-img" alt="">
     <?php endforeach; ?>
-    <div class="zoomit_viewer"></div>
+    </div>
 </div>
