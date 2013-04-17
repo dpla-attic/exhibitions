5e78b3c98615f41b67bfa3ad21c0980709e6f41d
Anton Yazovskiy <yazovsky@gmail.com>
2013-04-17 14:43:45 -0400

    zoom.it: use "update_cache_param" for AJAX images viewer

--- plugins/Zoomit/views/shared/common/zoomit.php
+++ plugins/Zoomit/views/shared/common/zoomit.php
@@ -1,11 +1,12 @@
 <?php
 $unique_id = "zoomit_".hash("md4", $images[0]->getWebPath('original'))
+$cache_param = html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->zoomit->update_cache_param);
 ?>
 
 <div class="zoomit" id="<?=$unique_id?>" data-api-url="<?php echo ZoomitPlugin::API_URL; ?>">
     <div class="zoomit_viewer">
     <?php foreach($images as $image): ?>
-    	<img src="<?php echo html_escape($image->getWebPath('original'))."?param=".html_escape(Zend_Registry::get('bootstrap')->getResource('Config')->dpla->zoomit->update_cache_param); ?>" data-original="<?php echo html_escape($image->getWebPath('original')); ?>" class="zoomit-image tmp-img" alt="">
+    	<img src="<?php echo html_escape($image->getWebPath('original'))."?param=".$cache_param; ?>" data-original="<?php echo html_escape($image->getWebPath('original'))."?param=".$cache_param; ?>" class="zoomit-image tmp-img" alt="">
     <?php endforeach; ?>
     </div>
 </div>
