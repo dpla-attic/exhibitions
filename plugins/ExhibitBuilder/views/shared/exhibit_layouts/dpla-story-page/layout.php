dpla-attic/exhibitions

commit 6c171fb4b9eb813c0b4ecc8974b6f011618ad167

bug #25484

plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-story-page/layout.php
@@ -14,13 +14,13 @@
                                 echo files_for_item(array(), array('class'=>'item-file'), $item['item']);
                             }
                             ?>
+                            <span data-id="<?=$unique_id?>" class="show-item-details cboxElement"><span>i</span></span>
                         </div>
 
                          <?php $json = get_dpla_api_object(dpla_get_field_value_by_name($item, 'Has Version')); ?>
                          <?php $imageTitle = $value = $json ? dpla_get_field_value_by_arrayname($json, array('sourceResource', 'title'))
 						     : dpla_get_field_value_by_name($item, "Title"); ?>                        
                         <?php $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item'])) ?>
-                        <span data-id="<?=$unique_id?>" class="show-item-details cboxElement"><span>i</span></span>
                         <div class="caption">
                             <?=$item['caption']?>
                         </div>

plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-theme-page/layout.php
@@ -14,13 +14,13 @@
                                 echo files_for_item(array(), array('class'=>'item-file'), $item['item']);
                             }
                             ?>
+                            <span data-id="<?=$unique_id?>" class="show-item-details cboxElement"><span>i</span></span>
                         </div>
                         
                          <?php $json = get_dpla_api_object(dpla_get_field_value_by_name($item, 'Has Version')); ?>
                          <?php $imageTitle = $value = $json ? dpla_get_field_value_by_arrayname($json, array('sourceResource', 'title'))
                              : dpla_get_field_value_by_name($item, "Title"); ?>                        
                         <?php $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item'])) ?>
-                        <span data-id="<?=$unique_id?>" class="show-item-details cboxElement"><span>i</span></span>
                         <div class="caption">
                             <?=$item['caption']?>
                         </div>

themes/dpla/css/main.css
@@ -2513,11 +2513,15 @@ aside h5 { margin: 5px 0 1em; }
     transition: 0.3s ease;
 }
 .show-item-details {
-    position: static;
-    border: 1px solid #fff;
     font-size: 1.4em;
-    margin: 1% 2%;
+    top: 15px;
+    left: 15px;
+}
+.video-mp4 + .show-item-details {
+    position: static;
+    border: none;
     float: right;
+    margin: 1.5%;
 }
 .moreInfo.hover {
     border-color: #fff;
@@ -2662,6 +2666,10 @@ html[xmlns] .slides {display: block;}
 .flex-direction-nav .flex-next {background-position: right 0; right: 17px; }
 .flex-direction-nav .flex-prev {right: 48px;}
 .flex-direction-nav .flex-disabled {opacity: .3!important; filter:alpha(opacity=30); cursor: default;}
+
+.plugin-content {
+    position: relative;
+}
 
 .plugin-content,
 .plugin-content .item-file,
@@ -3039,6 +3047,9 @@ footer .social li span { height: 31px; border-right: 1px solid #000000; border-b
 
 .caption {
     padding: 1% 2%;
+}
+.slide-Container .slides .caption {
+    clear: none;
 }
 .slides img { width: 100%; }
 

commit 8b608bc43916e1139af01da0e92bf90c324db058

Set current item variable properly (refs #19)
Fixes unassigned variable issue introduced by 6c171fb4b9eb813c0b4ecc8974b6f011618ad167

plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-story-page/layout.php
@@ -8,6 +8,7 @@
                     <li data-thumb="<?=$item['file_uri_square'] ?>" class="flexslider-slide">
                         <div class="plugin-content">
                             <?php
+                            $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item']));
                             if ($media = get_plugin_hook_output('public_items_show', array('view' => get_view(), 'item' => $item['item']))) {
                                 echo $media;
                             } else {
@@ -20,7 +21,6 @@
                          <?php $json = get_dpla_api_object(dpla_get_field_value_by_name($item, 'Has Version')); ?>
                          <?php $imageTitle = $value = $json ? dpla_get_field_value_by_arrayname($json, array('sourceResource', 'title'))
 						     : dpla_get_field_value_by_name($item, "Title"); ?>                        
-                        <?php $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item'])) ?>
                         <div class="caption">
                             <?=$item['caption']?>
                         </div>

plugins/ExhibitBuilder/views/shared/exhibit_layouts/dpla-theme-page/layout.php
@@ -8,6 +8,7 @@
                     <li data-thumb="<?=$item['file_uri_square'] ?>" class="flexslider-slide">
                         <div class="plugin-content">
                             <?php
+                            $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item']));
                             if ($media = get_plugin_hook_output('public_items_show', array('view' => get_view(), 'item' => $item['item']))) {
                                 echo $media;
                             } else {
@@ -20,7 +21,6 @@
                          <?php $json = get_dpla_api_object(dpla_get_field_value_by_name($item, 'Has Version')); ?>
                          <?php $imageTitle = $value = $json ? dpla_get_field_value_by_arrayname($json, array('sourceResource', 'title'))
                              : dpla_get_field_value_by_name($item, "Title"); ?>                        
-                        <?php $unique_id = "itemDetailsBox_".hash("md4", exhibit_builder_exhibit_item_uri($item['item'])) ?>
                         <div class="caption">
                             <?=$item['caption']?>
                         </div>