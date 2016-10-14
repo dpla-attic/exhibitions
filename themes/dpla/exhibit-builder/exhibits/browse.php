70867754598cf48ffa67006c45515194ec5bafd7
Audrey Altman <audrey@dp.la>
2016-10-14 17:07:14 -0400

    show three featured exhibits

--- themes/dpla/exhibit-builder/exhibits/browse.php
+++ themes/dpla/exhibit-builder/exhibits/browse.php
@@ -53,6 +53,12 @@ echo head(array('title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'browse
             </div>
         <?php endif; ?>
 
+        <!-- Featured Item -->
+        <div id="featured-item">
+            <h2><?php echo __('Featured Item'); ?></h2>
+            <?php echo random_featured_items(3); ?>
+        </div><!--end featured-item-->
+
         <?php $exhibitCount = 0; ?>
         <div class='pop-columns' data-page='1'>
             <div class="moduleContainer threeCol">
